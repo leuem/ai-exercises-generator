320b8f871610cc6a1142ab3b8239493063a0c953
Konstantin Bobkov <mrglorf@gmail.com>
2023-12-27 05:39:25 +0400

    vite config added

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,12 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/ai-exercises-generator/",
   plugins: [react()],
   server: {
     port: 3000,
   },
+  build: {
+    outDir: "./build"
+  }
 });
